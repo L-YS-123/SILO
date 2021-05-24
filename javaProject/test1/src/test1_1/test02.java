da62b02e977d65ad53f232414c6d333f170f91b5
L-YS-123 <yoonsunglee123@gmail.com>
2021-05-24 15:37:53 +0900

    ...

--- javaProject/test1/src/test1_1/test02.java
+++ javaProject/test1/src/test1_1/test02.java
@@ -1,10 +1,10 @@
-package test1;
+package test1_1;
 
-public class test01 {
+public class test02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("수정1");
+
 	}
 
 }
