389c30136c9206efe3695f2b05a57eec63b3c5ba
L-YS-123 <yoonsunglee123@gmail.com>
2021-05-25 16:08:08 +0900

    Update Member.java

--- javaProject/MyProject/src/Member.java
+++ javaProject/MyProject/src/Member.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
 public class Member {
-	public static void main(String[] args) {			
-		
-		String name="이윤성";
-		int age=26;
-		double height=170.0d;
-		boolean hasBook=true;
-		
-		System.out.println(name);
-		System.out.println(age);
-		System.out.println(height);
-		System.out.println(hasBook);		
-		
-		System.out.printf(" %s, %d, %f, %b ", name, age, height, hasBook);
-	}  
+	static Scanner scanner = new Scanner(System.in);
+	static int year = scanner.nextInt();
+
+	public static void testMethod() {
+
+		if ((2021 - year) < 15 || (2021 - year) > 64) {
+			System.out.println("무료예방접종이 가능합니다.");
+		} else {
+			System.out.println("무료접종 대상이 아닙니다.");
+		}
+
+		if ((2021 - year) > 19) {
+			if ((2021 - year) % 2 == 0) {
+				System.out.println("검사 대상입니다.");
+			} else {
+				System.out.println("검사 대상이 아닙니다.");
+				if ((2021 - year) > 39) {
+					System.out.println("무료 암 검사 대상입니다.");
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		testMethod();
+	}
 }
