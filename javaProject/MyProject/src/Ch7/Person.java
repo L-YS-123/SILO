acbc6afff928671cf58f15d8ca9646344a530ce7
L-YS-123 <yoonsunglee123@gmail.com>
2021-05-26 20:49:01 +0900

    Update Person.java

--- javaProject/MyProject/src/Ch7/Person.java
+++ javaProject/MyProject/src/Ch7/Person.java
@@ -1,23 +1,19 @@
 package Ch7;
 //Person 이라는 클래스를 정의해봅시다.
+
 //① 이름을 저장하는 변수, 주민등록번호를 저장하는 변수를 정의해봅시다.
 //② 인사하는 메소드를 정의해봅시다.
 //- “안녕하세요. 저는 OOO입니다. 00살 입니다.”라는 문자열이 출력하도록 정의합시다.
 
 public class Person {
-	String name;
-	String regiNum;
-	int age;
-	
-	public Person() {
-		name = "철수";
-		regiNum = "960000-1000000";
-		age = 121 - Integer.parseInt(regiNum.substring(0, 2));
+
+	Person() {
+		String name = "";
+		String regiNum = "960000-10000000";
+//		int age = 121 - Integer.parseInt(regiNum, 2);
 	}
-	
-	public void hello() {
-		System.out.printf("안녕하세요. 저는 %s입니다.\n", name);
-		System.out.printf("저는 %s년생입니다.\n", regiNum.substring(0, 2));
-		System.out.println();
-	}			
+
+	public void hello(String name,String regiNum) {
+		System.out.println("안녕하세요. 저는 " + name + "입니다." + regiNum + "살 입니다.");
+	}
 }
