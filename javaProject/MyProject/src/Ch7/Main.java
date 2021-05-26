303d19ba9d77b6036c6c9d2029840301b8baa4b6
L-YS-123 <yoonsunglee123@gmail.com>
2021-05-26 20:48:59 +0900

    Update Main.java

--- javaProject/MyProject/src/Ch7/Main.java
+++ javaProject/MyProject/src/Ch7/Main.java
@@ -5,13 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 //		 ① Person 클래스를 상속받은 Male클래스와 Female클래스를 이용해서 인스턴스를 생성해 봅시다.
 //		 ② 생성된 인스턴스들을 이용해서 메소드를 호출해봅시다.
-		Person p = new Person();
-		Male m = new Male();
-		Female fm = new Female();
+		Person p1 = new Person();
 		
-		p.hello();		
-		m.hello();		
-		fm.hello();
+		p1.hello("김철수", "960000-10000000");
 	}
-
 }
