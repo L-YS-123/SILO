d70b2699602363a16840eb029c3159f4ed0cf98b
L-YS-123 <yoonsunglee123@gmail.com>
2021-05-26 10:35:53 +0900

    Update Member.java

--- javaProject/MyProject/src/Ch4/Member.java
+++ javaProject/MyProject/src/Ch4/Member.java
@@ -1,10 +1,13 @@
+package Ch4;
+
 import java.util.Scanner;
 
 public class Member {
-	static Scanner scanner = new Scanner(System.in);
-	static int year = scanner.nextInt();
+	
+	Scanner scanner = new Scanner(System.in);	// scanner는 main 구동부에서 선언하는게 좋다
+	int year = scanner.nextInt();	// 입력이 안 되었을 때 실행이 안된다고(아무 반응이 없다고) 착각할 수도 있기 때문
 
-	public static void testMethod() {
+	public void testMethod() {
 
 		if ((2021 - year) < 15 || (2021 - year) > 64) {
 			System.out.println("무료예방접종이 가능합니다.");
@@ -25,7 +28,8 @@ public class Member {
 	}
 
 	public static void main(String[] args) {
-		testMethod(); 
+		 Member t = new Member(); 
+		 t.testMethod();
 	}
 }
 
