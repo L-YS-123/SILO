cf16ec97cc965dd01961b427c86b12add88124ad
L-YS-123 <yoonsunglee123@gmail.com>
2021-05-31 17:38:33 +0900

    Update Ex3.java

--- javaProject/MyProject/src/Interface/Ex3.java
+++ javaProject/MyProject/src/Interface/Ex3.java
@@ -1,11 +1,11 @@
 //3.다형성의 특징으로 상위 타입인 인터페이스 타입의 참조변수에 인터페이스를 구현한 클래스 타입의 인스턴스를 참조하는 코드를 작성해 봅시다.
 package Interface;
 
-public class ex3 {
+public class Ex3 {
 	
 	public static void main(String[] args) {
 		
-		Calculator calc = new ex2();
+		Calculator calc = new Ex2();
 		calc.act();
 
 	}
