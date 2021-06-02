8b3df5f21177a7e7b816f44b326e439c0169894c
L-YS-123 <yoonsunglee123@gmail.com>
2021-06-02 12:41:40 +0900

    Update ByteFileCopy.java

--- javaProject/MyProject/src/ex/io/ByteFileCopy.java
+++ javaProject/MyProject/src/ex/io/ByteFileCopy.java
@@ -10,9 +10,9 @@ public class ByteFileCopy {
 
 			// 1.파일을 바이너리 데이터로 읽어올 스트림 생성 : FileInputStream
 			// FileInputStream fileInputStream = new FileInputStream("orginal.pdf");
-			InputStream fileInputStream = new FileInputStream("orginal.pdf");
+			InputStream in = new FileInputStream("orginal.pdf");
 			// 2.파일을 쓰기위한 스트림 생성 : FileOutputStream
-			FileOutputStream fileOutputStream = new FileOutputStream("copy.pdf");
+			FileOutputStream out = new FileOutputStream("copy.pdf");
 
 			// 카피한 데이터의 크기
 			int copyByte = 0;
@@ -20,21 +20,21 @@ public class ByteFileCopy {
 			int bData = -1;
 
 			while (true) {
-				bData = System.in.read();
+				bData = in.read();
 
 				// 탈출의 조건 : 파일의 모든 데이터를 읽은 경우
 				if (bData == -1) {
 					break;
 				}
 				// 출력: 파일에 데이터를 쓴다
-				System.out.write(bData);
+				out.write(bData);
 				copyByte++;
 
 			}
 
 			// 스트림 닫기
-			System.in.close();
-			System.out.close();
+			in.close();
+			out.close();
 
 			System.out.println("복사완료! 복사된 바이트 사이즈는 : " + copyByte + "byte");
 
