464dcec71500c36c6b501c9f8971824f3ef460bd
L-YS-123 <yoonsunglee123@gmail.com>
2021-06-04 19:12:14 +0900

    Update CollectionFrameWork3.java

--- javaProject/MyProject/src/Ch14/CollectionFrameWork3.java
+++ javaProject/MyProject/src/Ch14/CollectionFrameWork3.java
@@ -2,6 +2,8 @@ package Ch14;
 
 public class CollectionFrameWork3 {
 
+	
+	
 	public static void main(String[] args) {
 		/*
 		 * 3.TreeSet<E>을 이용해서 팀 이름순으로 정렬하고, 같은 팀의 선수들은 이름 순으로 정렬하고, 같은 이름의 선수는 번호 순으로
@@ -9,7 +11,16 @@ public class CollectionFrameWork3 {
 		 * 
 		 * 4.축구선수의 번호를 key로 하고 축구선수 인스턴스를 저장하는 Map<K,V> 인스턴스를 이용해서 프로그램을 만들어봅시다.
 		 */
-
+		FootballPlayer fp = new FootballPlayer(null, 0, null, 0);
+		
+		String name = fp.name;
+		int number = fp.number;
+		String team = fp.team;
+		int age = fp.age;
+		
+		
+		
+		
 	}
 
 }
