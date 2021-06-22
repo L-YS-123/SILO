db1847cab5619aa0b98cf5ce2d5d69da04bbad24
L-YS-123 <yoonsunglee123@gmail.com>
2021-06-23 00:22:03 +0900

    Create SaleManager.java

---
+++ Sale/src/Selling/SaleManager.java
@@ -0,0 +1,17 @@
+package Selling;
+
+import java.sql.Connection;
+
+public class SaleManager {
+
+	void inputData() {
+		
+		Connection conn = null;
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String id = "hr";
+		String pw = "tiger";
+		
+	}
+	
+}
