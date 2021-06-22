929824d51af8dfebbf21ae07fb1b77bce33a0d9f
L-YS-123 <yoonsunglee123@gmail.com>
2021-06-22 22:06:41 +0900

    Create SaleMain.java

---
+++ Sale/src/Selling/SaleMain.java
@@ -0,0 +1,15 @@
+package Selling;
+
+public class SaleMain {
+
+	public static void main(String[] args) {
+		
+		SaleInfoDao dao = new SaleInfoDao();
+		
+		Order order = new Order();
+		order.Ordering();
+		
+		
+	}
+
+}
