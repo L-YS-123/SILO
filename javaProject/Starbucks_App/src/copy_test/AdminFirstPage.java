a977112bd355546978883e68bf5c58f5802f5aec
L-YS-123 <yoonsunglee123@gmail.com>
2021-06-24 18:18:44 +0900

    Update AdminFirstPage.java

--- javaProject/Starbucks_App/src/copy_test/AdminFirstPage.java
+++ javaProject/Starbucks_App/src/copy_test/AdminFirstPage.java
@@ -5,7 +5,7 @@ public class AdminFirstPage {
 	public static void main(String[] args) {
 		
 		AppMain appMain = new AppMain();
-		appMain.Submain();
+		appMain.SubMain(); //로그인 화면을 불러오는 메소드를 호출합니다
 	}
 	
 }
