2e3110d0de9d965f2c235cb39f31497c015cfe15
L-YS-123 <yoonsunglee123@gmail.com>
2021-06-24 16:54:24 +0900

    Update AdminPage.java

--- javaProject/Starbucks_App/src/copy_test/AdminPage.java
+++ javaProject/Starbucks_App/src/copy_test/AdminPage.java
@@ -8,27 +8,43 @@ public class AdminPage {
 	static MemberManager memManager = new MemberManager(MemberDao.getInstance());
 	static SaleManager saleManager = new SaleManager(SaleDao.getInstance());
 	static MenuManager menuManager = new MenuManager(MenuDao.getInstance());
+	static AppMain appMain = new AppMain();
 
 	void calling() {
-		System.out.println("관리자 화면");
-		System.out.println("1. 회원정보보기");
-		System.out.println("2. 판매정보보기");
-		System.out.println("3. 메뉴정보보기");
+		
+		
 		Scanner sc = new Scanner(System.in);
+		
 
-		int num2 = Integer.parseInt(sc.nextLine().trim());
-
-		switch (num2) {
-		case 1:
-			adminManager.memList();
-		case 2:
-//			saleManager.saleList();
-			adminManager.saleList();
-		case 3:
-//			menuManager.menuList();
-			adminManager.menuList();
-			break;
+		while(true) {
+			System.out.println("관리자 화면");
+			System.out.println("0. 첫 번재 화면으로 돌아가기");
+			System.out.println("1. 회원정보보기");
+			System.out.println("2. 판매정보보기");
+			System.out.println("3. 메뉴정보보기");
+			
+			int num2 = Integer.parseInt(sc.nextLine().trim());
+			
+			switch (num2) {
+			case 0:
+				appMain.Submain();
+				break;
+			case 1:
+				adminManager.memList();
+				break;
+			case 2:
+//				saleManager.saleList();
+				adminManager.saleList();
+				break;
+			case 3:
+//				menuManager.menuList();
+				adminManager.menuList();
+				break;
+			}
 		}
+			
+		
+
 	}
 
 }
