6acaa9f93b8ea00899fa66e8517af6dfa5184410
L-YS-123 <yoonsunglee123@gmail.com>
2021-07-13 18:22:06 +0900

    .

--- WEB/NCS_jQuery/ncs.js
+++ WEB/NCS_jQuery/ncs.js
@@ -23,32 +23,48 @@ $(document).ready(function () {
     // regForm 캐스팅 , 캐스팅: 태그를 문서객체로 만드는 것
     var regForm = $('#regForm');
 
-    regForm.onsubmit = function () {
 
-        // 배열에 사용자 정보를 추가
-        members.push(new Member(userid.val(), pw.val(), userName.val()));
+    var regExp1 = /^[a-zA-Z0-9]{4,12}$/; //아이디
 
-        // 저장
-        localStorage.setItem('members', JSON.stringify(members));
+    regForm.on('submit', function (userId) {
 
-        alert('등록되었습니다.');
 
-        console.log(userid.val());
-        console.log(pw.val());
-        console.log(repw.val());
-        console.log(userName.val());
+        if (!regExp1.test(userid.val())) {
+            alert('적절하지 않은 아이디 입니다.');
+            return false;
+        } else {
+            // 배열에 사용자 정보를 추가
+            members.push(new Member(userid.val(), pw.val(), userName.val()));
 
-        console.log('회원 리스트', members);
+            // 저장
+            localStorage.setItem('members', JSON.stringify(members));
 
-        // form 초기화
-        this.reset();
+            alert('등록되었습니다.');
 
-        // 테이블 세팅
-        // setList();
+            console.log(userid.val());
+            console.log(pw.val());
+            console.log(repw.val());
+            console.log(userName.val());
 
-        // return false;
+            console.log('회원 리스트', members);
 
-    }
+            // form 초기화
+            this.reset();
+
+            // 테이블 세팅
+            setList();
+
+            return false;
+
+        }
+
+
+
+    })
+
+
+
+
 
 });
 
@@ -57,7 +73,7 @@ function setList() {
 
 
     // table 의 tbody 캐스팅
-    var list = document.querySelector('#list');
+    var list = $('#list');
 
     var tbody = '<tr>';
     tbody += '  <th>순번(index)</th>';
@@ -87,10 +103,103 @@ function setList() {
         }
     }
 
-
-    list.innerHTML = tbody;
-    // $('#list').html('tbody');
+    $('#list').html(tbody);
 
 
 
 }
+
+
+// 배열의 요소 삭제 함수
+function deleteMember(index) {
+    // alert(index+' 인덱스의 요소를 삭제합니다.');
+
+    var chk = confirm('삭제하시겠습니까?');
+
+    //배열의 index요소 삭제
+    //splice(index, count) : index에서 시작해서 count만큼의 요소를 삭제하고 남은 요소를 반환(?)
+    //splice(index,1)
+    if (confirm('삭제하시겠습니까?')) {
+        members.splice(index, 1);
+        alert('삭제되었습니다.');
+        //테이블 리스트를 갱신
+        setList();
+    }
+
+}
+
+
+
+
+// 배열의 요소 수정함수
+function editMember(index) {
+
+    // 수정 폼 영역이 노출되어야 한다.
+    document.querySelector('#editFormArea').style.display = 'block';
+
+    // alert(index + ' 인덱스의 요소를 수정합니다.');
+
+    // 전달받은 index값으로 members 배열의 객체 하나를 얻을 수 있다
+    console.log(index, members[index])
+
+    // editForm의 태그들의 value값을 세팅해보자
+    var editUserId = document.querySelector('#editId');
+    var editPw = document.querySelector('#editPw');
+    var editRePw = document.querySelector('#editRePw');
+    var editName = document.querySelector('#editName');
+    var editIndex = document.querySelector('#index');
+
+    // 이전 데이터를 form에 세팅
+    editUserId.value = members[index].userId;
+    editPw.value = members[index].pw;
+    editRePw.value = members[index].pw;
+    editName.value = members[index].userName;
+    editIndex.value = index;
+
+    document.querySelector('#editForm').onsubmit = function () {
+
+        // var member = new Member(editUserId.value, editPw.value, editName.value);
+        // console.log(editIndex.value, member);
+
+
+        // 비밀번호와 비밀번호 확인이 같은지 체크
+        if (editPw.value != editRePw.value) {
+            alert('비밀번호와 비밀번호확인이 일치하지 않습니다.');
+            return false;
+        }
+
+
+
+        if (!confirm('수정하시겠습니까?')) {
+            return false;
+        }
+
+        members[editIndex.value].pw = editPw.value;
+        members[editIndex.value].userName = editName.value;
+
+        //저장
+        localStorage.setItem('members', JSON.stringify(members));
+
+        alert('수정되었습니다!');
+
+        // 테이블 세팅
+        setList();
+
+        editMemberClose();
+
+        return false;
+    }
+
+}
+
+//닫기
+function editMemberClose() {
+    document.querySelector('#editFormArea').style.display = 'none';
+}
+
+
+
+
+
+
+
