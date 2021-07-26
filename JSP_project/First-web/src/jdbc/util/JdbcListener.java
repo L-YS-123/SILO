854421f9c61f5fa153025ba4ad0e3cea5dc9d8ad
L-YS-123 <yoonsunglee123@gmail.com>
2021-07-26 12:34:28 +0900

    .

---
+++ JSP_project/First-web/src/jdbc/util/JdbcListener.java
@@ -0,0 +1,20 @@
+package jdbc.util;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class JdbcListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
