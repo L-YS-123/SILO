27ded6a3ac7f357f63d77f57b18c90d3191b8ef9
L-YS-123 <yoonsunglee123@gmail.com>
2021-07-26 16:18:25 +0900

    .

--- JSP_project/First-web/src/filter/CharacterEncodingFilter.java
+++ JSP_project/First-web/src/filter/CharacterEncodingFilter.java
@@ -1,33 +1,56 @@
 package filter;
 
 import java.io.IOException;
-
+import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Servlet Filter implementation class CharacterEncodingFilter
+ */
+@WebFilter(
+		dispatcherTypes = {DispatcherType.REQUEST }
+					, 
+		urlPatterns = { 
+				"/*", 
+				"/request/*"
+		}, 
+		initParams = { 
+				@WebInitParam(name = "encoding", value = "utf-8")
+		})
 public class CharacterEncodingFilter implements Filter {
 
-	@Override
+	private String encoding;
+	
+    
 	public void destroy() {
 		// TODO Auto-generated method stub
-
 	}
 
-	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
-			throws IOException, ServletException {
-		// TODO Auto-generated method stub
 
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		httpRequest.setCharacterEncoding(encoding);
+		
+		chain.doFilter(request, response);
+		
 	}
 
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-
+	public void init(FilterConfig fConfig) throws ServletException {
+		String paramValue = fConfig.getInitParameter("encoding");
+		if(paramValue != null) {
+			this.encoding = "utf-8"; 
+		} else {
+			this.encoding = paramValue;
+		}
 	}
 
 }
